030dd170ca13fcfd806a4c55d4ebe731d54bbe69
robin <robinvierich@gmail.com>
2019-05-15 15:02:24 -0400

    adds back Effects class for use in examples

---
+++ GOAP/Examples/Effects.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace SwordGC.AI.Actions
+{
+    /// <summary>
+    /// This can be used to store easy constants, included constants are used in examples
+    /// </summary>
+    public class Effects
+    {
+        public const string KNOCKED_OUT_PLAYER = "IsKnockedOut";
+        public const string HAS_OBJECT = "HasObject";
+        public const string IS_GONER = "IsGoner";
+        public const string SAME_TEAM = "SameTeam";
+        public const string DASHED_AT = "DashedAt";
+    }
+}
